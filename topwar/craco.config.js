cde780ae8c9da43d67001780d400ad4cfd463445
hiphop5782 <zdlsz@hanmail.net>
2023-10-06 23:39:46 +0900

    craco 추가
    ```sh
    npm install @craco/craco --save
    ```

---
+++ topwar/craco.config.js
@@ -0,0 +1,12 @@
+// craco.config.js
+module.exports = {
+    // 여기에 원하는 설정을 추가할 수 있습니다.
+    // 예를 들어, ESLint 설정을 수정하려면 다음과 같이 추가합니다.
+    eslint: {
+        configure: {
+            rules: {
+                // ESLint 규칙을 추가하거나 수정합니다.
+            },
+        },
+    },
+};
