e47305fd47e30b445089850d57f42a06a2779fd2
hiphop5782 <zdlsz@hanmail.net>
2023-10-09 14:19:11 +0900

    font 추가

--- topwar/src/components/screen/calculator/SkillCalculator.js
+++ topwar/src/components/screen/calculator/SkillCalculator.js
@@ -1,6 +1,14 @@
+
+
 function SkillCalculator() {
     return (
-        <h1>SkillCalculator</h1>
+        <>
+            <div className="row">
+                <div className="col">
+                    <h1>전속 스킬 계산기</h1>
+                </div>
+            </div>
+        </>
     );
 }
 
