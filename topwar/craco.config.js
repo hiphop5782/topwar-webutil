158004d4506759b4e18b4f0023265b8230a4c082
hiphop5782 <zdlsz@hanmail.net>
2023-10-09 17:31:36 +0900

    CRACO 빌드 경로 변경

--- topwar/craco.config.js
+++ topwar/craco.config.js
@@ -1,4 +1,6 @@
 // craco.config.js
+const path = require('path');
+
 module.exports = {
     // 여기에 원하는 설정을 추가할 수 있습니다.
     // 예를 들어, ESLint 설정을 수정하려면 다음과 같이 추가합니다.
@@ -6,6 +8,13 @@ module.exports = {
         configure: {
             rules: {
                 // ESLint 규칙을 추가하거나 수정합니다.
+            },
+        },
+    },
+    webpack:{
+        configure:{
+            output:{
+                path:path.resolve(__dirname, 'docs')
             },
         },
     },
