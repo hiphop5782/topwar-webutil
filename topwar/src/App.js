30551453a9a8e7db856f8bd4c607b89d79ffc0a4
hiphop5782 <zdlsz@hanmail.net>
2023-10-09 12:08:08 +0900

    bootstrap, bootswatch 적용
    - 기본 화면 제거
    - 메뉴 추가(bootswatch template)

--- topwar/src/App.js
+++ topwar/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+//loading bootstrap + bootswatch
+import 'bootswatch/dist/sandstone/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.esm.js';
+
+//loading components
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu></Menu>
     </div>
   );
 }
