5525f45170e18f79028f5139d66721755db5256f
hiphop5782 <zdlsz@hanmail.net>
2025-01-17 18:28:00 +0900

    타이탄 재련 시뮬레이션 구현중

--- topwar/src/components/screen/simulator/TitanRefineSimulator.js
+++ topwar/src/components/screen/simulator/TitanRefineSimulator.js
@@ -3,38 +3,87 @@ import partsList from "@src/assets/json/titan/titan-parts-types.json";
 import gearNames from "@src/assets/json/titan/titan-gear-names.json";
 import gearOptionList from "@src/assets/json/titan/titan-gear-options.json";
 import { useCallback, useEffect, useState } from "react";
+import { FaCheck } from "react-icons/fa"
+import { FaXmark } from "react-icons/fa6";
 
 const TitanRefineSimulator = () => {
     const [parts, setParts] = useState('pistol');
-    const [gearOptions, setGearOptions] = useState([
-        {title:'해군 데미지 증가', value:2.5},
-        {title:'공군 데미지 증가', value:3.1},
-        {title:'육군 데미지 증가', value:2.4}
-    ]);
-    const [materialOption, setMaterialOption] = useState({title:'해군 데미지 증가', value:2.5});
+    const [gearOptions, setGearOptions] = useState([]);
+    const [materialOption, setMaterialOption] = useState({ title: '해군 데미지 증가', value: 2.5 });
+
+    const [editingOption, setEditingOption] = useState({title:'', value:0.0});
 
     //파츠가 변경되면 기어옵션을 변경
-    useEffect(()=>{
-        if(!parts) return;
-        
-        //setGearOptions(gearOptionList[parts][0])
+    useEffect(() => {
+        if (!parts) return;
+
+        console.log(gearOptionList[parts][3]);
+        const option = gearOptionList[parts][3];
+        const mean = parseFloat(parseFloat((option.min + option.max) / 2).toFixed(2));
+        setGearOptions([{
+            title:option.title,
+            value: mean
+        }]);
     }, [parts]);
-    
-    const addGearOptions = useCallback((e)=>{
-        const option = gearOptionList[parts][0];
-        const mean = (option.min + option.max) / 2;
-        setGearOptions(prev=>{
-            if(prev.length === 3) return prev;
-            return [...prev, {title:option.title, value:parseFloat(mean.toFixed(2))}]
+
+    const addGearOptions = useCallback((e) => {
+        const option = gearOptionList[parts][3];
+        const mean = parseFloat(parseFloat((option.min + option.max) / 2).toFixed(2));
+        setGearOptions(prev => {
+            if (prev.length === 3) return prev;
+            return [...prev, { title: option.title, value: mean }]
         });
     }, [gearOptions]);
-    const removeGearOptions = useCallback((e)=>{
-        setGearOptions(prev=>{
-            if(prev.length === 1) return prev;
-            return prev.filter((opt, idx)=>idx < prev.length-1)
+    const removeGearOptions = useCallback((e) => {
+        setGearOptions(prev => {
+            if (prev.length === 1) return prev;
+            return prev.filter((opt, idx) => idx < prev.length - 1)
         });
     }, [gearOptions]);
 
+    const changeGearOption = useCallback((opt, idx) => {
+        setGearOptions(prev => prev.map((option, index) => {
+            if (index === idx) {
+                const obj = { ...option, edit: true };
+                setEditingOption(obj)
+                return obj;
+            }
+            return {...option, edit:false};
+        }));
+    }, [editingOption, gearOptions]);
+
+    const confirmChangeGearOption = useCallback((opt, idx)=>{
+        setGearOptions(prev => prev.map((option, index) => {
+            if (index === idx) {
+                return {...editingOption, edit:false};
+            }
+            return {...option};
+        }));
+    }, [editingOption, gearOptions]);
+    const cancelChangeGearOption = useCallback((opt, idx)=>{
+        setGearOptions(prev => prev.map((option, index) => {
+            if (index === idx) {
+                return { ...option, edit: false };
+            }
+            return option;
+        }));
+    }, [gearOptions]);
+    
+    const changeEditingOptionTitle = useCallback(e=>{
+        setEditingOption(prev=>({
+            ...editingOption,
+            title:e.target.value
+        }));
+    }, [editingOption]);
+    const changeEditingOptionValue = useCallback(e=>{
+        const value = e.target.value;
+        if(isNaN(value)) return;
+
+        setEditingOption(prev=>({
+            ...editingOption,
+            value:parseFloat(parseFloat(value).toFixed(2))
+        }));
+    }, [editingOption]);
     return (<>
         <h1>타이탄 재련</h1>
         <hr />
@@ -65,11 +114,28 @@ const TitanRefineSimulator = () => {
                             </div>
                             <div className="card-body" style={{ minWidth: '70%' }}>
                                 <h5 className="card-title" style={{ color: colorList['gold'] }}>{gearNames[parts]}</h5>
-                                {gearOptions.map((opt, idx)=>(
-                                <div className="card-text row mb-1" key={idx}>
-                                    <span className="col-8 text-begin text-truncate">{opt.title}</span>
-                                    <span className="col-4 text-end text-truncate">{opt.value}%</span>
-                                </div>
+                                {gearOptions.map((opt, idx) => (
+                                    <div className="card-text row mb-1" key={idx}>
+                                        {opt.edit === true ? (<>
+                                            <div className="input-group">
+                                                <select className="form control form-control-sm col-8" onChange={changeEditingOptionTitle} value={editingOption.title}>
+                                                    {gearOptionList[parts].map(option=>(
+                                                        <option>{option.title}</option>
+                                                    ))}
+                                                </select>
+                                                <input type="number" className="form-control form-control-sm col-4 text-end" value={editingOption.value} 
+                                                            onChange={changeEditingOptionValue} min={0} step={0.01}/>
+                                                <span className="ms-1">%</span>
+                                            </div>
+                                            <div className="text-end">
+                                                <FaCheck className="text-success fs-6" onClick={e=>confirmChangeGearOption(opt, idx)}/>
+                                                <FaXmark className="text-danger ms-2 fs-6" onClick={e=>cancelChangeGearOption(opt, idx)}/>
+                                            </div>
+                                        </>) : (<>
+                                            <span className="col-8 text-begin text-truncate" role="button" onClick={e => changeGearOption(opt, idx)}>{opt.title}</span>
+                                            <span className="col-4 text-end text-truncate" role="button" onClick={e => changeGearOption(opt, idx)}>{opt.value.toFixed(2)}%</span>
+                                        </>)}
+                                    </div>
                                 ))}
                             </div>
                         </div>
